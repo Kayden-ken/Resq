385c57fb3506d721edd48c1c6b0a816dbdcc4a8d
Kint Sala <kintzkiesala@gmail.com>
2026-07-06 11:42:12 +0800

    Simplify entrypoint

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,14 +2,9 @@
 
 set -e
 
-echo "Clearing Laravel caches..."
-php artisan config:clear
-php artisan cache:clear
-php artisan route:clear
-php artisan view:clear
-
 echo "Running database migrations..."
 php artisan migrate --force
 
-echo "Starting Apache..."
+echo "Migration finished."
+
 exec apache2-foreground
